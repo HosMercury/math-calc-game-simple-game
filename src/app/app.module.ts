b0a3e8589b8854e68188bf6db62c3c9f5816607a
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2021-01-26 16:01:31 +0200

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EquationComponent } from './equation/equation.component';
+import { AnswerDirective } from './answer.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EquationComponent, AnswerDirective],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
